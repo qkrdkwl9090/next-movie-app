754abb47610ab80542838f493bfb3338c6b1f34c
qkrdkwl9090 <qkrdkwl9090@naver.com>
2023-12-06 16:41:42 +0900

    feat(.env): move api token in .env

--- api.ts
+++ api.ts
@@ -1,11 +1,10 @@
 const BASE_URL = `https://api.themoviedb.org/3`;
-
+const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWYyYTMyZDMwNTA4Nzk2ODllMDMxNWJmYTM0OWI1ZCIsInN1YiI6IjYwYWNmMGZmMGE1MTdjMDAyOTBlOTZkNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yI6PYHiiU2zcSljek6us_hiAi3ZVYygnGc-x8T_zfw4",
+    Authorization: `Bearer ${API_TOKEN}`,
   },
 };
 
